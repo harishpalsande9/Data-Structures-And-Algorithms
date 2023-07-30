31afa7719008a82f25aea950c43ba22fffbd54b7
harishpalsande9 <harishpalsande9@gmail.com>
2023-07-30 12:53:42 +0530

    Number and Start Patterns

--- Patterns/NumbersPatterns.java
+++ Patterns/NumbersPatterns.java
@@ -1,13 +1,39 @@
 
 package Patterns;
 
+import java.util.Scanner;
+
 public class NumbersPatterns {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("* ");
+
+    public static void Pattern1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println("");
         }
     }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            // Pattern1(n);
+            Pattern2(n);
+        }
+
+    }
+
 }
